56829852c3be82d07319b0daf2769cfb539098e8
Anders Langlands <anderslanglands@gmail.com>
2025-01-20 11:09:45 +1300

    add pyside6

---
+++ pyside6/6.7.2/package.py
@@ -0,0 +1,39 @@
+name = "pyside6"
+version = "6.7.2"
+
+requires = ["python-3.9+<3.13"]
+
+
+@early()
+def variants():
+    import os, ast
+
+    cook_variant = os.getenv("REZ_COOK_VARIANT")
+    if cook_variant:
+        # If we're building the package, we want to use the variant supplied to us
+        return [ast.literal_eval(cook_variant)]
+    else:
+        # Otherwise tell rez-cook what variants we are capable of building
+        return [
+            ["platform-linux", "arch-x86_64", "cxx11abi", "python"],
+            ["platform-windows", "arch-AMD64", "vs", "python"],
+        ]
+
+
+def env(var: str):
+    import platform
+
+    if platform.system() == "Windows":
+        return f"$env:{var}"
+    else:
+        return f"${var}"
+
+
+build_command = f"""
+rez-env python-{env('PYTHON_VERSION')} -- python -m pip install {name}=={version} --target="{{install_path}}" --use-pep517
+"""
+
+
+def commands():
+    env.PYTHONPATH.prepend("{root}")
+    env.PATH.prepend("{root}/bin")
